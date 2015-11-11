9f9e54f435f4a7a70b6ef1fd1c9344146704da1a
teabow <thibaud.bourgeois@gmail.com>
2015-11-11 16:45:38 +0100

    add event bus to data manager

--- app/src/main/java/com/app/sample/sampleapp/data/DataManager.java
+++ app/src/main/java/com/app/sample/sampleapp/data/DataManager.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.app.sample.sampleapp.data.preferences.PreferencesHelper;
 import com.app.sample.sampleapp.data.service.ServiceFactory;
 import com.app.sample.sampleapp.data.service.movies.MoviesService;
+import com.squareup.otto.Bus;
 
 /**
  * Created by thibaud.bourgeois on 10/11/2015.
@@ -16,19 +17,25 @@ public class DataManager {
 
     private MoviesService moviesService;
 
+    private Bus eventBus;
+
     public DataManager(Context context) {
         this.preferencesHelper = new PreferencesHelper(context);
         this.moviesService = new ServiceFactory<MoviesService>()
                 .createServiceInterface(MoviesService.class, MoviesService.BASE_URL);
+        this.eventBus = new Bus();
     }
 
     public MoviesService getMoviesService() {
-        return moviesService;
+        return this.moviesService;
     }
 
     public PreferencesHelper getPreferencesHelper() {
+        return this.preferencesHelper;
+    }
 
-        return preferencesHelper;
+    public Bus getEventBus() {
+        return this.eventBus;
     }
 
 }
